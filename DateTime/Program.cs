1f093f02f7459cf58b4e2804b6f25437b005fb54
Keyner de Avila <kddeavila3@gmail.com>
2025-04-16 18:39:43 -0500

    Add string parsing and date time projects

---
+++ DateTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+DateTime dateOfBirth = new DateTime(2005, 12, 03);
+Console.WriteLine(dateOfBirth.DayOfWeek);
+
+// Computed values
+DateTime today = DateTime.Today;
+DateTime now = DateTime.Now;
+DateTime utcNow = DateTime.UtcNow;
+
+Console.WriteLine(today);
+Console.WriteLine(now);
+Console.WriteLine(utcNow);
+
+// DateTime parsing
+
+DateTime localDate = DateTime.Parse("03.12.2005");
+DateTime usDate = DateTime.Parse("12/03/2005", new CultureInfo("es-US"));
+
+Console.WriteLine($"Localdate: {localDate}, usDate: {usDate}");
+
+// DateTime formatting
+
+Console.WriteLine(dateOfBirth.ToString());
+Console.WriteLine(dateOfBirth.ToString(new CultureInfo("es-US")));
+Console.WriteLine(dateOfBirth.ToString("yyyy-MM-dd"));
---
+++ StringParsing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+string luckyNumber = "16";
+
+// Convert to int
+
+int paseLuckyNumber = int.Parse(luckyNumber);
+Console.WriteLine($"Lucky number: {paseLuckyNumber}");
+
+// Culture info
+CultureInfo cultureInfo = new CultureInfo("de-DE");
+
+double temperature = double.Parse("30.7", cultureInfo);
+double temperature2 = double.Parse("30.,7", cultureInfo);
+
+Console.WriteLine($"Temp: {temperature}");
+Console.WriteLine($"Temp2: {temperature2}");
