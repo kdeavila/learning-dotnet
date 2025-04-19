e981b87e382fd2d348adb4b1a8643a64d3977133
Keyner de Avila <kddeavila3@gmail.com>
2025-04-19 13:59:13 -0500

    Add Polymorphism and advanced variable scope on classes and methods

---
+++ AdvancedVariablesScope/Program.cs
@@ -0,0 +1,27 @@
+﻿Book book = new Book("Cien años de soledad", "Gabriel García Márquez");
+book.PrintSummary();
+Console.WriteLine($"Lenght title: {book.GetTitleLength()}");
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public Book(string title, string author)
+    { 
+        Title = title;
+        Author = author;
+    }
+
+    public void PrintSummary()
+    {
+        string summary = $"{Title} by {Author}";
+        Console.WriteLine(summary);
+    }
+
+    public int GetTitleLength()
+    {
+        string titleLenghtWitoutSpaces = Title.Replace(" ", "");
+        return titleLenghtWitoutSpaces.Length;
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,37 @@
+﻿Person person = new Person();
+Employee employee = new Employee();
+Customer customer = new Customer();
+
+// or use the following lines to create instances
+
+//Person person = new Person();
+//Person employee = new Employee();
+//Person customer = new Customer();
+
+person.Greeting();
+employee.Greeting();
+customer.Greeting();
+
+public class Person
+{
+    public virtual void Greeting()
+    {
+        Console.WriteLine("Hello, I am a regular person.");
+    }
+}
+
+public class Employee : Person
+{
+    public override void Greeting()
+    {
+        Console.WriteLine("Hello, I am an employee.");
+    }
+}
+
+public class Customer : Person
+{
+    public override void Greeting()
+    {
+        Console.WriteLine("Hello, I am a customer.");
+    }
+}
