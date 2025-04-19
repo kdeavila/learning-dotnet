dee60396829606b6bf713977dda29feed3fac0c4
Keyner de Avila <kddeavila3@gmail.com>
2025-04-19 00:19:04 -0500

    Add BasicClass and HouseQuest projects with Person class demonstration

---
+++ BasicClass/Program.cs
@@ -0,0 +1,73 @@
+﻿// Instance a class
+
+Person person1 = new Person("Keyner");
+Person person2 = new Person("Brayan");
+
+Console.WriteLine(person1.Name);
+Console.WriteLine(person2.Name);
+
+// Change the name of the first person
+
+person1.Name = "Keyner de Ávila";
+Console.WriteLine(person2.Name);
+
+person1.PrintGreeting();
+person2.PrintGreeting();
+
+// Call the method GetGreeting
+
+Console.WriteLine(person1.GetGreeting());
+Console.WriteLine(person2.GetGreeting());
+
+// Call the method Sum from the Math class
+
+Math math = new Math();
+int result = math.Sum(14, 16);
+Console.WriteLine(result);
+
+//class Person
+//{
+//    private string _name;
+
+//    public Person()
+//    {
+//        _name = "Keyner";
+//    }
+
+//    public string Name
+//    {
+//        get { return _name; }
+//        set { _name = value; }
+//    }
+
+//}
+
+
+// 
+class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+
+    public void PrintGreeting()
+    {
+        Console.WriteLine($"Hello, {Name}");
+    }
+
+    public string GetGreeting()
+    {
+        return $"Hello, {Name}";
+    }
+}
+
+public class Math
+{
+    public int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ HouseQuest/Program.cs
@@ -0,0 +1,31 @@
+﻿House house1 = new House(1990, 100);
+House house2 = new House(2010, 200);
+Console.WriteLine($"House 1 can be sold: {house1.CanBeSold()}");
+Console.WriteLine($"House 2 can be sold: {house2.CanBeSold()}");
+
+class House
+{
+    public House(int year, int meters)
+    { 
+        Year = year;
+        Meters = meters;
+    }
+
+    public int Year { get; set; }
+    public int Meters { get; set; }
+
+    private int HowOld()
+    {
+        DateTime dateNow = DateTime.Now;
+        int actualYear = dateNow.Year;
+
+        return actualYear - Year;
+    }
+
+    public bool CanBeSold()
+    {
+        int age = HowOld();
+
+        return age >= 15;
+    }
+}
