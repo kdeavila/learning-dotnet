4d758323efc7e941d4f1049ea447ab954e80d34d
Keyner de Avila <kddeavila3@gmail.com>
2025-04-21 00:24:58 -0500

    Add const project

---
+++ Intermediate/Const/Program.cs
@@ -0,0 +1,10 @@
+﻿const int age = 32;
+// age = 20; // Don't reassign a const variable
+
+public class  Person
+{
+    const int Age = 21;
+    const string Name = "C# Course";
+    const string Version = "2";
+    const string FullProductName = $"{Name} {Version}";
+}
