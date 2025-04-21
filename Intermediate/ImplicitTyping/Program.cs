84e471a1b97e02030b74f6ee22091d4551f9f60d
Keyner de Avila <kddeavila3@gmail.com>
2025-04-21 00:20:00 -0500

    Add intermediate folder and Implicit typing project

---
+++ Intermediate/ImplicitTyping/Program.cs
@@ -0,0 +1,18 @@
+﻿var name = "John Doe";
+var age = 30;
+
+var person = new Person(name, age);
+
+Console.WriteLine($"Person Name: {person.Name}, Age: {person.Age}");
+
+public class Person
+{
+    public string Name { get; set; } = "Default Name";
+    public int Age { get; set; } = 25;
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
