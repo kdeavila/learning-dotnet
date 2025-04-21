095add9f59c51962af8b49b01895b0d4606b2620
Keyner de Avila <kddeavila3@gmail.com>
2025-04-21 11:24:00 -0500

    Add structs and inmutable structs with readonly

---
+++ Intermediate/Structs/Program.cs
@@ -0,0 +1,26 @@
+﻿var rectangle = new Rectangle(200, 300);
+var inmutableRectangle = new InmutableRectangle(200, 300);
+
+public struct Rectangle
+{
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public double Width { get; set; }
+    public double Height { get; set; }
+}
+
+public readonly struct InmutableRectangle
+{
+    public InmutableRectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public double Width { get; init; }
+    public double Height { get; init; }
+}
