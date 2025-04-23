61f33923cc77a5e9499f017e8cd6c8c51b76fed4
Keyner de Avila <kddeavila3@gmail.com>
2025-04-22 23:47:12 -0500

    Add exception handling API

---
+++ Intermediate/ExceptionHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿//string content = "";
+
+//try
+//{
+//    content = File.ReadAllText("doc.txt");
+//    Console.WriteLine("The file has been read.");
+//}
+//catch (FileNotFoundException exception)
+//{
+//    Console.WriteLine($"ERROR: The file '{exception.FileName}' could not be found");
+//} finally
+//{
+//    Console.WriteLine($"The content length is: {content.Length}");
+//}
+
+var filename = "doc.txt";
+
+if (File.Exists(filename))
+{
+    var content = File.ReadAllText(filename);
+    Console.WriteLine(content);
+}
+else
+{
+    Console.WriteLine($"ERROR: The file '{filename}' could not be found");
+}
