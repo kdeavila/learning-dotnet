8491506f2b09ea143dc4b837966b74836c21f5de
Keyner de Avila <kddeavila3@gmail.com>
2025-04-23 22:18:50 -0500

    Add array list project

---
+++ Arrays and Collections/ArrayList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+var arrayList = new ArrayList();
+
+var day = 22;
+arrayList.Add(day);
+
+var person1 = new Person("Keyner");
+arrayList.Add(person1);
+
+var firstElement = arrayList[0];
+Console.WriteLine($"First element: {firstElement}"); // Output: 22
+
+foreach (var item in arrayList)
+{
+    if (item is Person)
+    {
+        var Person = item as Person;
+        if (Person != null)
+        {
+            Console.WriteLine($"Person: {Person.Name}");
+        }
+    } else
+    {
+        Console.WriteLine($"Item: {item}");
+    }
+}
+        
+public class Person
+{
+    public string Name { set; get; }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+}
