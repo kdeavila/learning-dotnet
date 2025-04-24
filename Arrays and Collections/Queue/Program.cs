b455a4f7b2b3b35aa841a6dbd8830c9b307a8e59
Keyner de Avila <kddeavila3@gmail.com>
2025-04-24 00:03:49 -0500

    Add Queue project

---
+++ Arrays and Collections/Queue/Program.cs
@@ -0,0 +1,10 @@
+﻿var queue = new Queue<string>();
+
+queue.Enqueue("First");
+queue.Enqueue("Second");
+queue.Enqueue("Third");
+
+while (queue.Count > 0)
+{
+    Console.WriteLine(queue.Dequeue());
+}
