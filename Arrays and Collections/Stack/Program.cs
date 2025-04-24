a6c763cb1fa5ebf6d0ec67ac888bad1744c32d2a
Keyner de Avila <kddeavila3@gmail.com>
2025-04-23 23:12:10 -0500

    Add stack project: Stack<T>

---
+++ Arrays and Collections/Stack/Program.cs
@@ -0,0 +1,18 @@
+﻿var countries = new Stack<string>();
+
+countries.Push("USA");
+countries.Push("Canada");
+countries.Push("Mexico");
+countries.Push("Brazil");
+countries.Push("Argentina");
+
+var lastCountry = countries.Pop();
+Console.WriteLine($"Last country added: {lastCountry}");
+
+var topCountry = countries.Peek();
+Console.WriteLine($"Top country in the stack: {topCountry}\n");
+
+foreach (var country in countries)
+{
+    Console.WriteLine($"Country in stack: {country}");
+}
