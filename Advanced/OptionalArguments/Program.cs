8a82735883480b23f731cd42649239d7914cba87
Keyner de Avila <kddeavila3@gmail.com>
2025-04-26 23:51:07 -0500

    Add optional parameters project

---
+++ Advanced/OptionalArguments/Program.cs
@@ -0,0 +1,22 @@
+﻿var person1 = new Person("Keyner", 19);
+Console.WriteLine(person1.LuckyNumber);
+
+var person2 = new Person("Keyner", age: 19, luckyNumber: 3);
+Console.WriteLine(person2.LuckyNumber);
+
+public class Person
+{
+    // Optional arguments must be at the end of the parameter list
+    public Person(string name, int age, string favoriteColor, int luckyNumber = 7)
+    {
+        Name = name;
+        Age = age;
+        FavoriteColor = favoriteColor;
+        LuckyNumber = luckyNumber;
+    }
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int LuckyNumber { get; set; }
+    public string FavoriteColor { get; set; }
+}
