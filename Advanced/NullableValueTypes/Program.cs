b0217a23cbc4230110c54868772be6b80ed32d8b
Keyner de Avila <kddeavila3@gmail.com>
2025-05-02 09:41:32 -0500

    Add nullable value types project

---
+++ Advanced/NullableValueTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿int? possibleValue = GetScore(42);
+
+Console.WriteLine(
+    possibleValue.HasValue
+    ? $"Value found: {possibleValue.Value}"
+    : "No value found"
+);
+
+DateTime? inputDate = ParseDate(null);
+Console.WriteLine(
+    inputDate.HasValue
+    ? $"Parsed date: {inputDate.Value}"
+    : "Invalid date format"
+);
+
+
+static int? GetScore(int? userId)
+{
+    if (userId == 42) return null;
+    return 100;
+}
+
+static DateTime? ParseDate(string? text)
+{
+    if (DateTime.TryParse(text, out var d))
+    {
+        return d;
+    }
+
+    return null;
+}
