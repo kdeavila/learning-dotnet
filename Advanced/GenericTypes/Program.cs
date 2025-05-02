83f6b3e26331c3a00674f04697a828fd9f9d8b1f
Keyner de Avila <kddeavila3@gmail.com>
2025-05-02 08:37:07 -0500

    Add generic types project

---
+++ Advanced/GenericTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿MyList<int> myList = new MyList<int>();
+myList.Add(1);
+myList.Add(2);
+Console.WriteLine(myList.GetByIndex(1));
+
+MyList<string> myList1 = new MyList<string>();
+myList1.Add("Hello");
+myList1.Add("World");
+Console.WriteLine(myList1.GetByIndex(0));
+
+
+public class MyList<T>
+{
+    public MyList(int initialCapacity = 4)
+    {
+        _items = new T[initialCapacity];
+    }
+
+    private T[] _items;
+    private int _counter = 0;
+
+    public void Add(T item)
+    {
+        if (_counter == _items.Length)
+        {
+            Array.Resize(ref _items, _items.Length * 2);
+        }
+        _items[_counter] = item;
+        _counter++;
+    }
+
+    public T GetByIndex(int index)
+    {
+        if (index < 0 || index >= _counter)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+        }
+        return _items[index];
+    }
+
+    public int Count => _counter;
+}
