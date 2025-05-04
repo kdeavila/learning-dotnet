1dff4a3ad81fbff70dbf3ef60b6ea0dda464acd5
Keyner de Avila <kddeavila3@gmail.com>
2025-05-04 09:01:13 -0500

    Add record types project

---
+++ Advanced/RecordTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿var person1 = new Person("Keyner", 19);
+var person2 = new Person("Keyner", 19);
+
+Console.WriteLine(person1);
+Console.WriteLine(person1 == person2
+    ? "They're equal"
+    : "They're not equal"
+    );
+
+var person3 = person1 with { Age = 20 };
+Console.WriteLine(person3);
+
+public record Person(string Name, int Age);
