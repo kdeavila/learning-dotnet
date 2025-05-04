79ce83e38662c44867138cdb5e576260f214e33e
Keyner de Avila <kddeavila3@gmail.com>
2025-05-04 08:19:17 -0500

    Add nullable reference types

---
+++ Advanced/NullableReferences/Program.cs
@@ -0,0 +1,26 @@
+﻿// Activate <Nullable> in the project file
+// <Nullable>enable</Nullable>
+
+string greetings = "Hola, Keyner";
+greetings = null;
+
+//Console.WriteLine(greetings.Length); // Warning: Dereference of a possibly null reference
+
+string? userName = GetUser();
+
+if (userName != null)
+{
+    Console.WriteLine($"User: {userName.Length} characters");
+}
+else
+{
+    Console.WriteLine("User name is null");
+}
+
+int size = userName!.Length;
+Console.WriteLine(size);
+
+static string? GetUser()
+{
+    return null;
+}
