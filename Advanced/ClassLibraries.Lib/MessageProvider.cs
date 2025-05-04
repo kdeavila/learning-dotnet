9aef2fe10d826a0aba9c2ecc33e4ef49afe07589
Keyner de Avila <kddeavila3@gmail.com>
2025-05-04 09:47:37 -0500

    Add class libraries and class libraries lib project

---
+++ Advanced/ClassLibraries.Lib/MessageProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassLibraries.Lib
+{
+    public class MessageProvider
+    {
+        public static void WriteMessage(string? message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                Console.WriteLine("No message provided.");
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
