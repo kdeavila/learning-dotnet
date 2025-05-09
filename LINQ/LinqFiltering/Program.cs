996fea3f0b85ac7e38ce931a0681e42e5304125a
Keyner de Avila <kddeavila3@gmail.com>
2025-05-08 19:59:23 -0500

    Add Linq filtering data project

---
+++ LINQ/LinqFiltering/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+var products = new[]
+{
+    new {Name = "T shirt ", Price = 19.99, InStock = true },
+    new {Name = "Jeans", Price = 49.99, InStock = true },
+    new {Name = "Sneakers", Price = 89.99, InStock = true },
+    new {Name = "Hat", Price = 15.99, InStock = false }, 
+    new {Name = "Socks", Price = 5.99, InStock = false },
+};
+
+
+// Filter products by availability
+var availables = products.Where(p => p.InStock);
+
+// Filter products by price
+var expensives = availables.Where(p => p.Price > 70);
+
+Console.WriteLine("Products in stock and price > 70");
+foreach (var p in expensives)
+{
+    Console.WriteLine($"{p.Name} - ${p.Price}");
+}
