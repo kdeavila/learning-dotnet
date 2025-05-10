c000d23ea74e1340d02d08da7cdc59dc7b14f148
Keyner de Avila <kddeavila3@gmail.com>
2025-05-10 08:19:24 -0500

    Add linq grouping project

---
+++ LINQ/LinqGrouping/Program.cs
@@ -0,0 +1,28 @@
+﻿var fruits = new List<Product>
+{
+    new Product { Name = "Apple", Category = "Fruit" },
+    new Product { Name = "Banana", Category = "Fruit" },
+    new Product { Name = "Carrot", Category = "Vegetable" },
+    new Product { Name = "Broccoli", Category = "Vegetable" },
+    new Product { Name = "Potato", Category = "Vegetable" },
+    new Product { Name = "Milk", Category = "Dairy" },
+    new Product { Name = "Cheese", Category = "Dairy" },
+};
+
+var groupedProducts = fruits.GroupBy(p => p.Category);
+
+foreach (var category in groupedProducts)
+{
+    Console.WriteLine($"\n{category.Key}:");
+
+    foreach (var p in category)
+    {
+        Console.WriteLine($" - {p.Name}");
+    }
+}
+
+public class Product
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+}
