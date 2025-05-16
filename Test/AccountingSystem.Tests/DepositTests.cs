738d93fde415fcc7200e3f67a9d81f34f44b0f85
Keyner de Avila <kddeavila3@gmail.com>
2025-05-15 22:31:18 -0500

    Add Deposit test automated project

---
+++ Test/AccountingSystem.Tests/DepositTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AccountingSystem;
+
+namespace AccountingSystem.Tests
+{
+    [TestClass]
+    public class DepositTests
+    {
+        [TestMethod]
+        public void Deposit_PositiveAmount_IncreasesBalance()
+        {
+            var account = new CheckingAccount(1, 1000m);
+            account.Deposit(500m);
+            Assert.AreEqual(1500m, GetBalance(account));
+        }
+
+        private decimal GetBalance(Account account)
+        {
+            var prop = typeof(Account).GetProperty("Balance", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic |
+              System.Reflection.BindingFlags.Public);
+
+            return (decimal)prop.GetValue(account)!;
+        }
+    }
+}
