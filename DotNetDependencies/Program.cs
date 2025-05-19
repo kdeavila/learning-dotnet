e14278a99040d8cc33a58a81a6e1efd9b0c96736
Keyner de Avila <kddeavila3@gmail.com>
2025-05-19 09:37:02 -0500

    Add dotnet dependencies using Humanizer library

---
+++ DotNetDependencies/Program.cs
@@ -0,0 +1,22 @@
+﻿using Humanizer;
+
+Console.WriteLine("- Quantities");
+HumanizeQuantities();
+
+Console.WriteLine("\n- Date/Time Manipulation");
+HumanizeDates();
+
+static void HumanizeQuantities()
+{
+    Console.WriteLine("animal".ToQuantity(0));
+    Console.WriteLine("house".ToQuantity(1));
+    Console.WriteLine("car".ToQuantity(2));
+}
+
+static void HumanizeDates()
+{
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
