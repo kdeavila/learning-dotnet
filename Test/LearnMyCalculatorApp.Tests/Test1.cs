7055a689bbecf7a3849a3e342a1fe2b8ad7ffefd
Keyner de Avila <kddeavila3@gmail.com>
2025-05-26 23:34:30 -0500

    Add new calculator test from Microsoft Learn and do some changes in the files name

---
+++ Test/LearnMyCalculatorApp.Tests/Test1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearnMyCalculatorApp;
+
+namespace LearnMyCalculatorApp.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void CalculatorNullTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsNotNull(calculator);
+
+            Assert.IsTrue(false);
+        }
+    }
+}
