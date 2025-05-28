7c83f713cb909c7eae47f9ad9b3ccca11727c239
Keyner de Avila <kddeavila3@gmail.com>
2025-05-28 09:44:56 -0500

    Add more tests to the calculator app test project

--- DotNetTests/LearnMyCalculatorApp.Tests/Test1.cs
+++ DotNetTests/LearnMyCalculatorApp.Tests/Test1.cs
@@ -12,5 +12,45 @@ namespace LearnMyCalculatorApp.Tests
             var calculator = new Calculator();
             Assert.IsNotNull(calculator);
         }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            var calculator = new Calculator();
+            int actual = calculator.Add(1, 1);
+            Assert.AreEqual(actual, 2);
+        }
+
+        [TestMethod]
+        public void SubtractTest()
+        {
+            var calculator = new Calculator();
+            int actual = calculator.Subtract(2, 1);
+            Assert.AreEqual(actual, 1);
+        }
+
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            var calculator = new Calculator();
+            int actual = calculator.Multiply(2, 1);
+            Assert.AreEqual(actual, 2);
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            var calculator = new Calculator();
+            var actual = calculator.Divide(2, 1);
+            Assert.AreEqual(actual, 2);
+        }
+
+        [TestMethod]
+        public void DivideByZeroTest()
+        {
+            var calculator = new Calculator();
+            var actual = calculator.Divide(2, 0);
+            Assert.IsNull(actual);
+        }
     }
 }
--- DotNetTests/LearnMyCalculatorApp/Calculator.cs
+++ DotNetTests/LearnMyCalculatorApp/Calculator.cs
@@ -6,11 +6,17 @@
         public int Subtract(int x, int y) => x - y;
         public int Multiply(int x, int y) => x * y;
 
-        public int Divide(int x, int y)
+        public int? Divide(int x, int y)
         {
-            if (y == 0)
-                throw new DivideByZeroException("Cannot divide by zero.");
-            return x / y;
+            try
+            {
+                return x / y;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+                return null;
+            }
         }
     }
 }
